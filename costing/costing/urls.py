4ac41add92cd2a499606eb9170355abff2a8235a
Justin Mearns <justin.mearns@gmail.com>
2023-07-28 15:58:35 -0600

    Updated index page

--- costing/costing/urls.py
+++ costing/costing/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth.views import LogoutView
 from django.urls import include, path
@@ -22,4 +24,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('', include('webapp.urls'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
